0aad08d4dee8f1ef52411fb58c2cc42fb92e3d31
Ting Zhou <limtingzhou@gmail.com>
2019-02-06 19:11:19 +0800

    Removes redundant url path

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const expressApp = require("./src/express").expressApp;
 
 bot.launch();
 
-expressApp.use(bot.webhookCallback("/secret-path"));
-bot.telegram.setWebhook("https://janet-habit-bot.now.sh/secret-path");
+expressApp.use(bot.webhookCallback("/"));
+bot.telegram.setWebhook("https://janet-habit-bot.now.sh/");
