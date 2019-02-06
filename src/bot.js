15ef4cd02eef023948df9e5ab3f04322307c2184
Ting Zhou <limtingzhou@gmail.com>
2019-02-06 19:14:53 +0800

    Modify folder name

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,11 @@
 // Create bot with commands
 const Telegraf = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const startCommand = require("./botCommands/start").startCommand;
-const checkInCommand = require("./botCommands/checkin").checkInCommand;
-const checkOutCommand = require("./botCommands/checkout").checkOutCommand;
-const helpCommand = require("./botCommands/help").helpCommand;
-const cheatDayCommand = require("./botCommands/cheatday").cheatDayCommand;
+const startCommand = require("./bot_commands/start").startCommand;
+const checkInCommand = require("./bot_commands/checkin").checkInCommand;
+const checkOutCommand = require("./bot_commands/checkout").checkOutCommand;
+const helpCommand = require("./bot_commands/help").helpCommand;
+const cheatDayCommand = require("./bot_commands/cheatday").cheatDayCommand;
 
 startCommand(bot);
 checkInCommand(bot);
