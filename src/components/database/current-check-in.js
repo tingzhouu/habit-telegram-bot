ca8c263359c6865105e7cf6dbd31430d63047cd5
Ting Zhou <limtingzhou@gmail.com>
2019-02-10 14:50:21 +0800

    update current check in function

--- src/components/database/current-check-in.js
+++ src/components/database/current-check-in.js
@@ -12,8 +12,11 @@ const currentCheckInLog = mongoose.model("CheckInLog", currentCheckInSchema);
 
 function logCheckIn(ctx) {
   const previousCheckInDetails = checkCheckedInUser(ctx);
+  console.log("start of previous check in details");
+  console.log(previousCheckInDetails);
+  console.log("end of previous check in details");
 
-  if (isCheckedIn) {
+  if (previousCheckInDetails) {
     ctx.reply(`Your previous check-in timestamp is ${getDateString(previousCheckInDetails.checkInTimeStamp)}`);
   } else {
     const newRequestLog = new currentCheckInLog({
