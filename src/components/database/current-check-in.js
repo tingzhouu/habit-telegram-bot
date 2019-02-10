81ca91eb93f665e55e4b85b2cc516de088637877
Ting Zhou <limtingzhou@gmail.com>
2019-02-10 15:00:05 +0800

    update current check in function

--- src/components/database/current-check-in.js
+++ src/components/database/current-check-in.js
@@ -10,41 +10,28 @@ const currentCheckInSchema = mongoose.Schema({
 
 const currentCheckInLog = mongoose.model("CheckInLog", currentCheckInSchema);
 
-async function logCheckIn(ctx) {
-  const previousCheckInDetails = await checkCheckedInUser(ctx);
-  console.log("start of previous check in details");
-  console.log(previousCheckInDetails);
-  console.log("end of previous check in details");
-
-  if (previousCheckInDetails) {
-    ctx.reply(`Your previous check-in timestamp is ${getDateString(previousCheckInDetails.checkInTimeStamp)}`);
-  } else {
-    const newRequestLog = new currentCheckInLog({
-      telegramID: ctx.from.id,
-      checkInTimeStamp: moment()
-    });
-  
-    newRequestLog.save(function(err) {
-      if (!err) {
-        console.log("successfully logged to DB");
-        ctx.reply("You have checked in successfully");
-      } else {
-        console.log(err);
-      }
-    });
-  }
-}
-
-function checkCheckedInUser(ctx) {
+function logCheckIn(ctx) {
+  const currentTimeStamp = moment();
   currentCheckInLog.findOne({ telegramID: ctx.from.id }, function(err, docs) {
     if (docs != null) {
       console.log("i see you have checked in");
+      ctx.reply(`Your previous check-in timestamp is ${getDateString(previousCheckInDetails.checkInTimeStamp)}`);
       console.log(docs);
-      return docs;
     } else {
       console.log("i see you have not checked in");
-      console.log(err);
-      return null;
+      const newRequestLog = new currentCheckInLog({
+        telegramID: ctx.from.id,
+        checkInTimeStamp: currentTimeStamp
+      });
+
+      newRequestLog.save(function(err) {
+        if (!err) {
+          console.log("successfully logged to DB");
+          ctx.reply(`Your check-in timestamp is ${getDateString(currentTimeStamp)}`);
+        } else {
+          console.log(err);
+        }
+      });
     }
   });
 }
