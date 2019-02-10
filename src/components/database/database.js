33db41af42ab92e7dc2e9b1d1b5b8a0aa3901d9d
Ting Zhou <limtingzhou@gmail.com>
2019-02-10 11:00:49 +0800

    Simple Heroku Site connect to DB

--- src/components/database/database.js
+++ src/components/database/database.js
@@ -5,7 +5,14 @@ function connectToDB() {
   mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-71gbh.mongodb.net/wikiDB?retryWrites=true`,
     { useNewUrlParser: true }
-  );
+  ).then(function() {
+    console.log(`User is ${process.env.DB_USER}`)
+    console.log("Connected to DB");
+  })
+  .catch(function(err) {
+    console.log("Failed to connect to DB");
+    console.log(err);
+  });
 }
 
 module.exports = {
