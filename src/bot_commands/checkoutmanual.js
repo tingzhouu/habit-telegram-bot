f69afcaab4c49adf9f44f670ada2e2086f5c5b66
Ting Zhou <limtingzhou@gmail.com>
2019-03-03 21:29:20 +0800

    Add status function for user to check on check-in status

--- src/bot_commands/checkoutmanual.js
+++ src/bot_commands/checkoutmanual.js
@@ -75,7 +75,6 @@ function getManualTimeStamp(ctx) {
 }
 
 function errorMessage(ctx) {
-  ctx.reply("dummy");
   ctx.reply(
     'For manual check-out, please use the following format with 24HR time: /check_out_manual DD/MM/YY HH:MM.\n\nExample: "/check_out_manual 22/04/19 18:23" will create a timestamp for 22 April 2019, 1823hrs'
   );
