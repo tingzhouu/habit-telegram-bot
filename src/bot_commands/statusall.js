5b87337e2688ea50648db3c8581cb1f527a571bc
Ting Zhou <limtingzhou@gmail.com>
2019-03-13 23:36:25 +0800

    Add status_all to view everyone's status

--- src/bot_commands/statusall.js
+++ src/bot_commands/statusall.js
@@ -6,7 +6,6 @@ const moment = require("moment");
 
 async function statusAllCommand(bot) {
   bot.command(["status_all", "status_all@janet_habit_bot"], (ctx) => {
-    ctx.reply("dummy");
     userLog.find(function(err, docs) {
       docs.forEach(async function(user) {
         const statusString = await checkInStatus(user.telegramID);
